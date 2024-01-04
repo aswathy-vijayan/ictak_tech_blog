928d5b28b69a57db17a97eefa540b4cf464f1d99
dinahkurian <dinahkurian@outlook.com>
2024-01-04 21:15:31 +0530

    Updated Non-registered user activities

---
+++ src/main/java/org/ictak/constants/AutomationConstants.java
@@ -0,0 +1,14 @@
+package org.ictak.constants;
+
+public class AutomationConstants 
+{
+	public static String s1="Home";
+	public static String s2="Tech News and Gadgets. . .";
+	public static String s3="Latest Articles";
+	public static String s4="Share Feedback";
+	public static String abt= "ABOUT US";
+	public static String cont="CONTACT US";
+	public static String in="Login";
+	public static String up="Signup form";
+
+}
---
+++ src/test/java/org/ictak/test/NonRegTestBase.java
@@ -0,0 +1,39 @@
+package org.ictak.test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
+
+public class NonRegTestBase {
+	WebDriver driver;
+	public static Properties prop=null;
+	@BeforeMethod
+	public void setup() throws IOException
+	{
+		prop=new Properties();
+		FileInputStream ip=new FileInputStream("C:\\Core_Java\\Projects\\ictproject\\ictak_tech_blog\\src\\test\\resources\\config.properties");
+		prop.load(ip);
+		driver=new ChromeDriver();
+		driver.get(prop.getProperty("url"));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+	}
+	@AfterMethod
+	public void closing() throws IOException
+	{
+		driver=new ChromeDriver();
+		driver.quit();
+	}
+	
+	
+	//TechBlog
+
+}
