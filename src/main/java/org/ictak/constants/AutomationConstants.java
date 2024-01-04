e5a5798f1e3494431b10f5bfb88ab1c02b5aed3c
Aswathy V <aswathy.v03@gmail.com>
2024-01-04 19:45:21 +0530

    Add automation constants

---
+++ src/main/java/org/ictak/constants/AutomationConstants.java
@@ -0,0 +1,20 @@
+package org.ictak.constants;
+
+public class AutomationConstants {
+	public static String postTitle = "Sample test title";
+	public static String imageUrl = "https://archive.org/download/placeholder-image/placeholder-image.jpg";
+	public static String invalidImageUrl = "https://example.com/placeholder-image.jpg";
+	public static String category = "JENKINS";
+	public static String postContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut egestas, sem eu lobortis suscipit, justo mi maximus leo, eget accumsan metus metus id neque.";
+
+	public static String postTitleNew = "New sample test title";
+	public static String imageUrlNew = "https://archive.org/download/placeholder-image/placeholder-image.jpg";
+	public static String invalidImageUrlNew = "https://testsite.com/image.jpg";
+	public static String postContentNew = "Lorem ipsum dolor sit amet";
+
+	public static String trainerPostSuccessMessage = "New Post Added";
+	public static String postUpdateSuccessMessage = "Updated Successfully";
+	public static String userPostSuccessMessage = "Post send for approval from admin";
+
+	public static String userPostDeleteMessage = "Post deleted successfully";
+}
