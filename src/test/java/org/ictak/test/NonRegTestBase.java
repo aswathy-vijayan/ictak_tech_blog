ecd20b2c2b79b9329f7fa762c4d6bc91c1a0eeeb
dinahkurian <dinahkurian@outlook.com>
2024-01-05 22:36:50 +0530

    package name updated

--- src/test/java/org/ictak/test/NonRegTestBase.java
+++ src/test/java/org/ictak/test/NonRegTestBase.java
@@ -7,6 +7,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
@@ -21,7 +22,16 @@ public class NonRegTestBase {
 		prop=new Properties();
 		FileInputStream ip=new FileInputStream("C:\\Core_Java\\Projects\\ictproject\\ictak_tech_blog\\src\\test\\resources\\config.properties");
 		prop.load(ip);
-		driver=new ChromeDriver();
+//		driver=new ChromeDriver();		
+	
+		
+		String browserName = prop.getProperty("browser");
+		if (browserName.equalsIgnoreCase("chrome")) {
+			
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		}
 		driver.get(prop.getProperty("url"));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().window().maximize();
